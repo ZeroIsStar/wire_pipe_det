25b6959f111190847ab6e668e62ab4c486d554de
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-07-02 12:10:56 +0800

    Update wireandpipe_detection.cpp

--- src/wireandpipe_detection.cpp
+++ src/wireandpipe_detection.cpp
@@ -276,7 +276,7 @@ WireAndPipeDetectionNode::WireAndPipeDetectionNode()
     this->declare_parameter<double>("camera_yaw", 0.0);
     this->declare_parameter<double>("camera_roll", 0.0);
 
-    this->declare_parameter<std::string>("yolo_model_path", "/capella/lib/python3.10/site-packages/wire_pipe_det/src/6_26.onnx");
+    this->declare_parameter<std::string>("yolo_model_path", "/capella/lib/python3.10/site-packages/wire_pipe_det/src/7_1_1.onnx");
     this->declare_parameter<std::vector<std::string>>("class_names", std::vector<std::string>{"wire", "water_pipe"});
     this->declare_parameter<int>("wire_class_id", 0);
     this->declare_parameter<int>("water_pipe_class_id", 1);
